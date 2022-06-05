8da20ef32230a870e7d150c69a1ada060a66fc01
Avidu <avidu97@gmail.com>
2022-06-06 00:53:52 +0530

    let see

--- pages/hotels/[slug].js
+++ pages/hotels/[slug].js
@@ -4,45 +4,46 @@ import Link from "next/link";
 import { GraphQLClient } from "graphql-request";
 import { gql } from "graphql-request";
 
-const graphcms = new GraphQLClient(
-  "https://api-ap-south-1.graphcms.com/v2/cl41i5arf37e101xs9dmq4usk/master"
-);
-
-const QUERY = gql`
-  {
-    hotels {
-      slug
-      name
-    }
-  }
-`;
-
 export async function getStaticProps() {
-  const { hotels } = await graphcms.request(QUERY);
+  const graphcms = new GraphQLClient(
+    "https://api-ap-south-1.graphcms.com/v2/cl41i5arf37e101xs9dmq4usk/master"
+  );
+
+  const { hotels } = await graphcms.request(
+    `
+      { 
+        hotels {
+          slug
+          name
+        }
+      }
+    `
+  );
 
   return {
     props: {
       hotels,
     },
-    fallback: 'blocking'
   };
 }
 
-export default function Hotels({ hotels }) {
+export default function HotelsSingle({ hotels }) {
   console.log(hotels);
   return (
     <div>
       <Head></Head>
-      <div>
-        <h1>Hi this is hotels page</h1>
+      <>
         <div>
-          <ul>
-            {hotels.map(({ slug, title }) => (
-              <li key={slug}>{title}</li>
+          <h1>Hi this is hotels page</h1>
+          <div>
+            {hotels.map(({ slug, name }) => (
+                <Link key={slug} href={`/hotels/${slug}`}>
+                  <a>{name}</a>
+                </Link>
             ))}
-          </ul>
+          </div>
         </div>
-      </div>
+      </>
     </div>
   );
 }
