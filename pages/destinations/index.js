cb341654bb65a6450050f87c5d392722401f7132
Avidu <avidu97@gmail.com>
2022-06-06 01:23:39 +0530

    both index pages are getting data

--- pages/destinations/index.js
+++ pages/destinations/index.js
@@ -1,14 +1,42 @@
-import Head from "next/head"
+import Head from "next/head";
+import { GraphQLClient } from "graphql-request";
+import { gql } from "graphql-request";
 
-export default function DestinationPage() {
-    return (
-        <div>
-            <Head>
+const graphcms = new GraphQLClient(
+  "https://api-ap-south-1.graphcms.com/v2/cl41i5arf37e101xs9dmq4usk/master"
+);
 
-            </Head>
-            <div>
-                <h1>This is destinations Home page</h1>
-            </div>
-        </div>
-    )
+const QUERY = gql`
+  {
+    destinations {
+      id
+      name
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const { destinations } = await graphcms.request(QUERY);
+
+  return {
+    props: {
+      destinations,
+    },
+  };
+}
+
+export default function DestinationPage({ destinations }) {
+  return (
+    <div>
+      <Head></Head>
+      <div>
+        <h1>This is destinations Home page</h1>
+        <ul>
+          {destinations.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
