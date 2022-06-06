57c92a152eb23df76e8db1e1542f25ef89ee24b8
Avidu <avidu97@gmail.com>
2022-06-06 14:36:12 +0530

    its time to add content to website

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,6 +14,8 @@ export default function Home() {
 
       <div>
         <h1>This is the Homepage</h1>
+        <h2><Link href="/hotels">Hotels</Link></h2>
+        <h2><Link href="/destinations">Destinations</Link></h2>
       </div>
     </div>
   )
