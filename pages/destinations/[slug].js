1f760c65aad54516a0cd108ff1c34d8f3d7202fe
Avidu <avidu97@gmail.com>
2022-06-06 12:17:11 +0530

    getting everything from the backend

--- pages/destinations/[slug].js
+++ pages/destinations/[slug].js
@@ -1,15 +1,60 @@
-import React from 'react'
+import React from "react";
 import Head from "next/head";
+import { GraphQLClient } from "graphql-request";
 
-export default function Destinations() {
-    return (
-        <div>
-            <Head>
+const graphcms = new GraphQLClient(
+  "https://api-ap-south-1.graphcms.com/v2/cl41i5arf37e101xs9dmq4usk/master"
+);
 
-            </Head>
-            <div>
-                <h1>Hi this is Destinations Page</h1>
-            </div>
-        </div>
-    )
+export async function getStaticProps({ params }) {
+  const { destination } = await graphcms.request(
+    `
+        query DestinationPageQuery($slug: String!) {
+          destination(where: { slug: $slug }) {
+            name
+            description
+          }
+        }
+      `,
+    {
+      slug: params.slug,
+    }
+  );
+
+  return {
+    props: {
+      destination,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const { destinations } = await graphcms.request(`
+      {
+        destinations {
+          slug
+          name
+        }
+      }
+    `);
+
+  return {
+    paths: destinations.map(({ slug }) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
+}
+
+export default function Destinations({ destination }) {
+  return (
+    <div>
+      <Head></Head>
+      <div>
+        <h1>Hi this is Destinations Page</h1>
+        <h2>{destination.name}</h2>
+        <p>{destination.description}</p>
+      </div>
+    </div>
+  );
 }
